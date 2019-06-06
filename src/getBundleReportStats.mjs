f68dfbff24410a1bb51fd5824249f58c44b73340
Sam Kelleher <sam@samkelleher.com>
2019-06-06 19:38:30 +0100

    feat(Bundle Report): Read directly from file system depending on setup.

--- src/getBundleReportStats.mjs
+++ src/getBundleReportStats.mjs
@@ -1,4 +1,45 @@
 import path from 'path';
+import fs from 'fs';
+
+const fsPromises = fs.promises;
+
+const fileExists = async path => {
+    try {
+        await fsPromises.access(path);
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            // File not exists
+            return false;
+        }
+        return null;
+    }
+    return true;
+};
+
+const readJSON = async path => {
+    let reportDataString;
+
+    try {
+        reportDataString = await fsPromises.readFile(path, {
+            encoding: 'utf8'
+        });
+    } catch (error) {
+        console.error(`Failed to read report at "${path}"`);
+        console.error(error);
+        return;
+    }
+
+    let reportData;
+    try {
+        reportData = JSON.parse(reportDataString)
+    } catch(error) {
+        console.error(`Failed to parse report at "${path}"`);
+        console.error(error);
+        return;
+    }
+
+    return reportData;
+};
 
 export default async function (cwd) {
     const version = process.env.APP_VERSION;
@@ -8,17 +49,23 @@ export default async function (cwd) {
         return undefined;
     }
 
-    const reportPath = path.join(cwd, `./reports/${version}-bundle-data.json`);
+    // The path the build container will mount the report.
+    let reportPath = path.join(cwd, `./${version}-bundle-data.json`);
 
-    let stats;
-    try {
-        stats = await import(reportPath);
-        stats = stats.default; // why?
-    } catch (error) {
-        console.error(`Unable to read stats file at ${reportPath}`);
-        console.error(error);
+    if (!await fileExists(reportPath)) {
+        // Look up in the reports directory
+        // This is when we run the tool directly in repo
+        reportPath = path.join(cwd, `./reports/${version}-bundle-data.json`);
+    }
+
+    if (!await fileExists(reportPath)) {
+        console.warn(`Did not find report file at ${reportPath}`);
         return;
     }
+
+    const stats = await readJSON(reportPath);
+
+    if (!stats) return;
 
     const friendlyAssets = stats.assets.map(asset => (
         {
